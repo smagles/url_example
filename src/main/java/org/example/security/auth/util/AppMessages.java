a2c6c7c3304513774ef7965c644c297038f989f3
smagles <lesialist@icloud.com>
2023-12-27 19:06:45 +0100

    AppMessages

---
+++ src/main/java/org/example/security/auth/util/AppMessages.java
@@ -0,0 +1,10 @@
+package org.example.security.auth.util;
+
+public class AppMessages {
+    public static final String INVALID_PASSWORD_MESSAGE = "Invalid password. Password must be at least 8 characters long and include at least one digit, one uppercase letter, and one lowercase letter.";
+    public static final String PASSWORD_MISMATCH_MESSAGE = "Password and confirm password do not match";
+    public static final String SUCCESS_MESSAGE = "Success";
+    public static final String INVALID_CREDENTIALS_MESSAGE = "Invalid username or password";
+    public static final String USERNAME_ALREADY_EXISTS_MESSAGE = "Username already exists. Please choose a different username.";
+
+}
