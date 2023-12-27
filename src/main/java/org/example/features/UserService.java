a0c431857154636f7a2a9ec7b6f38df69db4cbe3
smagles <lesialist@icloud.com>
2023-12-27 09:42:42 +0100

    UserService

--- src/main/java/org/example/features/UserService.java
+++ src/main/java/org/example/features/UserService.java
@@ -1,20 +1,9 @@
 package org.example.features;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import javax.management.relation.Role;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
-
 
 @Service
 @RequiredArgsConstructor
